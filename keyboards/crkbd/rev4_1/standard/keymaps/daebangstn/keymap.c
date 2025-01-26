1e6da3009b84789f4dd44c31191abc61e96afd09
daebangstn <gunho1107@naver.com>
2025-01-26 21:05:53 +0900

    remap

--- keyboards/crkbd/rev4_1/standard/keymaps/daebangstn/keymap.c
+++ keyboards/crkbd/rev4_1/standard/keymaps/daebangstn/keymap.c
@@ -1,3 +1,4 @@
+#include "quantum_keycodes.h"
 #include QMK_KEYBOARD_H
 #if __has_include("keymap.h")
     #include "keymap.h"
@@ -50,13 +51,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_UP,            KC_LEFT, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_BSPC,
         KC_CAPS, KC_A, KC_S, KC_D, KC_F, KC_G, KC_DOWN,         KC_RGHT, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT,
         KC_LNG1, KC_Z, KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, TD(TD_QUOTE_DEL),
-        KC_LCTL, MO(1), KC_LCTL,                                LALT_T(TD_CONSOLE), KC_LSFT, KC_LGUI
+        KC_LCTL, MO(1), KC_LCTL,                                LALT_T(TD_CONSOLE), KC_LSFT, LCTL_T(KC_LGUI)
         ),
     [1] = LAYOUT_split_3x6_3_ex2(
         KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_PGUP,      KC_LEFT, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_BSPC,
         KC_DOT, KC_1, KC_2, KC_3, KC_4, KC_5, KC_PGDN,          KC_RGHT, KC_6, KC_7, KC_8, KC_9, KC_0, KC_ENT, KC_PSCR,
         KC_NO, KC_INS, KC_DEL, KC_HOME, KC_END,                 KC_LCBR, KC_RCBR, KC_LPRN, KC_RPRN, KC_LBRC, KC_RBRC,
-        KC_NO, KC_NO, KC_LCTL,                                  KC_SPC, KC_NO, KC_NO
+        KC_NO, KC_NO, KC_LCTL,                                  KC_SPC, KC_NO, KC_LCTL
         )
 };
 
